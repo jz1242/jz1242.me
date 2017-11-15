d26ed06520a42fac33a65e24bcc9fdf09689f729
Jason <jz1242@gmail.com>
2017-11-15 16:53:45 -0500

    Site_v1

--- src/App.js
+++ src/App.js
@@ -1,36 +1,74 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import img from './face.jpg';
 import './App.css';
-import { Card, CardImg, CardText, CardBody, CardTitle, CardSubtitle, Button } from 'reactstrap';
+import { Jumbotron, Button } from 'reactstrap';
+import resume from './JasonZhangResume.pdf';
 class App extends Component {
   constructor(props) {
     super(props);
 
     this.toggle = this.toggle.bind(this);
     this.state = {
-      isOpen: false
+      dropdownOpen: false
     };
   }
+
   toggle() {
     this.setState({
-      isOpen: !this.state.isOpen
+      dropdownOpen: !this.state.dropdownOpen
     });
   }
   render() {
     return (
       <div className="App">
-        <Card>
-          <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97180&w=318&h=180" alt="Card image cap" />
-          <CardBody>
-            <CardTitle>Card title</CardTitle>
-            <CardSubtitle>Card subtitle</CardSubtitle>
-            <CardText>Some quick example text to build on the card title and make up the bulk of the card's content.</CardText>
-            <Button>Button</Button>
-          </CardBody>
-        </Card>
-        <a href="https://github.com/jz1242" class="icon fa fa-github fa-5x"></a>
-        <a href="https://github.com/jz1242" class="icon fa fa-linkedin fa-5x"></a>
-      </div>
+        <Jumbotron>
+          <img src={img} height="100" id='face' />
+          <h1 className="display-3">Hello, World! 
+          </h1>
+          <p className="lead">My name is Jason Zhang and I'm a Computer Engineering Major at Johns Hopkins University! I find tech facinating
+            and I want to use programming to solve all of my daily needs.
+          </p>
+          <hr className="my-2" />
+          <h3>Computers are my jam! I've got experience in...</h3>
+          <br></br>
+          <table width='100%'>
+            <tr>
+              <td>Java</td>
+              <td>Python</td>
+              <td>C++</td>
+              <td>C</td>
+            </tr>
+            <tr>
+              <td>MATLAB</td>
+              <td>HTML</td>
+              <td>CSS</td>
+              <td>JavaScript</td>
+            </tr>
+            <tr>
+              <td>ReactJS</td>
+              <td>React-Bootstrap-4</td>
+              <td>MeteorJS</td>
+              <td>NodeJS</td>
+            </tr>
+            <tr>
+              <td>Python Django</td>
+              <td>Scipy</td>
+              <td>Numpy</td>
+              <td>Pytorch</td>
+            </tr>
+            <tr>
+              <td>NGINX</td>
+              <td>Vagrant</td>
+              <td>MongoDB</td>
+              <td>MySQL</td>
+            </tr>
+          </table>
+          <br></br>
+          <a href="https://github.com/jz1242" class="icon fa fa-github fa-3x"> </a>
+          <a href="https://www.linkedin.com/in/jason-zhang-182991125/" class="icon fa fa-linkedin fa-3x"></a>
+          <a href={resume} class="icon fa fa-file fa-3x"></a>
+      </Jumbotron>  
+    </div>
     );
   }
 }
