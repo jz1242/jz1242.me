e2ec91605756cfc304de94d80f8cab4874d8df7a
jz1242 <jz1242@gmail.com>
2018-12-27 00:15:48 -0500

    updated site

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import img from './face.jpg';
 import './App.css';
 import { Jumbotron, Button } from 'reactstrap';
 import resume from './JasonZhangResume.pdf';
+
 class App extends Component {
   constructor(props) {
     super(props);
@@ -21,52 +22,28 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className="personalpic">
+          <img src={img} height="300" id='face' />
+          <div class="overlay">
+            <div class="text">Jason Zhang</div>
+          </div>
+        </div>
         <Jumbotron>
-          <img src={img} height="100" id='face' />
-          <h1 className="display-3">Hello, World! 
+          <h1>
+            Welcome to my site!
           </h1>
-          <p className="lead">My name is Jason Zhang and I'm a Computer Engineering Major at Johns Hopkins University! I find tech fascinating
-            and I want to use programming to solve all of my daily needs.
-          </p>
           <hr className="my-2" />
-          <h3>Computers are my jam! I've got experience in...</h3>
           <br></br>
-          <table width='100%'>
-            <tr>
-              <td>Java</td>
-              <td>Python</td>
-              <td>C++</td>
-              <td>C</td>
-            </tr>
-            <tr>
-              <td>MATLAB</td>
-              <td>HTML</td>
-              <td>CSS</td>
-              <td>JavaScript</td>
-            </tr>
-            <tr>
-              <td>ReactJS</td>
-              <td>React-Bootstrap-4</td>
-              <td>MeteorJS</td>
-              <td>NodeJS</td>
-            </tr>
-            <tr>
-              <td>Python Django</td>
-              <td>Scipy</td>
-              <td>Numpy</td>
-              <td>Pytorch</td>
-            </tr>
-            <tr>
-              <td>NGINX</td>
-              <td>Vagrant</td>
-              <td>MongoDB</td>
-              <td>MySQL</td>
-            </tr>
-          </table>
+          <p className="lead">I'm a Computer Science major at Johns Hopkins University. I have interests in Machine Learning, Distributed Systems/Networking,
+          <br></br>photography, gaming, and cooking!
+          </p>
           <br></br>
-          <a href="https://github.com/jz1242" class="icon fa fa-github fa-3x"> </a>
-          <a href="https://www.linkedin.com/in/jason-zhang-182991125/" class="icon fa fa-linkedin fa-3x"></a>
-          <a href={resume} class="icon fa fa-file fa-3x"></a>
+          <br></br>
+          <h4>Links to see more about me!</h4>
+          <a href="https://github.com/jz1242" class="icon fa fa-github fa-2x"> </a>
+          <a href="https://www.linkedin.com/in/jason-zhang-182991125/" class="icon fa fa-linkedin fa-2x"></a>
+          <a href="https://www.instagram.com/riceasianman/?hl=en" class="icon fa fa-instagram fa-2x"></a>
+          <a href={resume} class="icon fa fa-file fa-2x"></a>
       </Jumbotron>  
     </div>
     );
